ecdcef814970a5d5e1ebf2e5f7386ab467d952d1
Benjamin Babu <benjamin.babu2002@gmail.com>
2025-06-15 20:14:47 -0400

    updated header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,20 +3,56 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/R5C_1851.jpg'
 import HeaderSocials from './HeaderSocials'
+import { TypeAnimation } from 'react-type-animation';
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hi. My name is</h5>
-        <h1>Benjamin Babu</h1>
+        {/* <h5>Hi. My name is</h5>
+        <h1>Benjamin Babu</h1> */}
         {/*<h5 className="text-light">Fullstack Developer</h5>*/}
         {/* <CTA /> */}
+
+        <div className='container text-lines'>
+          <span className='text-line'>
+            <h1>Hi! I am Benjamin,</h1>
+          </span>
+        </div>
+        <br/>
+        <div className='container text-lines'>
+          <span className='text-line'>
+            <h1>and I make</h1>
+          </span>
+        </div>
+        <br/>
+        <div className='container text-lines'>
+          <span className='text-line'>
+            <h1>
+              <TypeAnimation
+                sequence={[
+                  500,
+                  'code',
+                  1000,
+                  'bugs',
+                  1000,
+                  'bad decisions :)',
+                  1000,
+                  'stuff.',
+                ]}
+                wrapper="span"
+                cursor={true}
+                style={{ display: 'inline-block' }}
+              />
+            </h1>
+          </span>
+        </div>
+
         <HeaderSocials />
 
-        <div className="me">
+        {/* <div className="me">
           <img src={ME} alt="me" />
-        </div>
+        </div> */}
 
         <a href="#about" className='scroll__down'>Enter Portfolio</a>
       </div>
